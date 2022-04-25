aa0483d7e37d918ef0d2167629c4f4df08b960e3
wael <waeelash155@gmail.com>
2022-04-25 16:44:13 -0400

    commit tree 7473814c734647a22023b025b54f27b9432f761e

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get '/students/1', to: "students#first"
+  get '/students/2', to: 'students#second'
+  get '/cheeses/:id', to: 'cheeses#show'
 end
